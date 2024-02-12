64907fd75376c615c5236ddefb4aeef5552d9804
Antoine Savreux <antoinesavreux06@gmail.com>
2024-02-12 09:47:32 +0100

    feat/skeleton created Header and Logo component

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import Logo from "./Logo";
+
+export default function Header() {
+  return (
+    <header>
+      {/* Logo */}
+      <Logo />
+      {/* ThemeSwitch */}
+      {/* NavMenu */}
+      header
+    </header>
+  );
+}
---
+++ src/components/Logo.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import logoImage from "@/../public/images/logo-light.png";
+
+export default function Logo() {
+  return <Image src={logoImage} alt="logo" />;
+}
