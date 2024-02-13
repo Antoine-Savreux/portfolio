458e9fc2dc816219545a11b7441dde25cb1df07c
Antoine Savreux <antoinesavreux06@gmail.com>
2024-02-13 11:44:38 +0100

    fix/skeleton removing Layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import Layout from "@/components/Layout";
 import "./globals.css";
 
@@ -21,8 +22,9 @@ export default function RootLayout({
       <body className={inter.className}>
         {/* header */}
         <Header />
-        <Layout>{children}</Layout>
+        {children}
         {/* footer */}
+        <Footer />
       </body>
     </html>
   );
--- src/components/Layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="bg-green-500 p-2">{children}</div>;
-}
