4e70b058f4f21b331be3217cd33959503c792c4a
Antoine Savreux <antoinesavreux06@gmail.com>
2024-02-15 13:48:07 +0100

    feat/homepage functional carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,35 +1,77 @@
+"use client";
+
 import ProjectCard from "./ProjectCard";
 import forumImage from "@/../public/images/forum.jpg";
 import weatherAppImage from "@/../public/images/weather-app.jpg";
 import websiteImage from "@/../public/images/website.jpg";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { useEffect, useState } from "react";
+
+const projectData = [
+  {
+    id: 1,
+    name: "website",
+    description: "a fully responsive website",
+    image: websiteImage,
+  },
+  {
+    id: 2,
+    name: "weatherApp",
+    description: "weather app",
+    image: weatherAppImage,
+  },
+  {
+    id: 3,
+    name: "forum",
+    description: "my personal view of forum",
+    image: forumImage,
+  },
+  {
+    id: 4,
+    name: "website2",
+    description: "a fully responsive website 2",
+    image: websiteImage,
+  },
+];
 
 export default function Carousel() {
-  const projectData = [
-    {
-      name: "website",
-      description: "a fully responsive website",
-      image: websiteImage,
-    },
-    {
-      name: "weatherApp",
-      description: "weather app",
-      image: weatherAppImage,
-    },
-    {
-      name: "forum",
-      description: "my personal view of forum",
-      image: forumImage,
-    },
-    {
-      name: "website2",
-      description: "a fully responsive website 2",
-      image: websiteImage,
-    },
-  ];
+  const [currentIndex, setCurrentIndex] = useState(3);
+
+  const filteredProject = projectData.filter(
+    (project) => project.id === currentIndex
+  );
+
+  const previousSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 1 ? 4 : prevIndex - 1));
+  };
+
+  const followingSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 4 ? 1 : prevIndex + 1));
+  };
+
   return (
-    <div className="">
+    <div className="relative">
+      {/* arrow buttons */}
+      <ChevronLeft
+        className="absolute left-0 bottom-1/2 text-secondary"
+        size={42}
+        onClick={previousSlide}
+      />
+      <ChevronRight
+        className="absolute right-0 bottom-1/2 text-secondary"
+        size={42}
+        onClick={followingSlide}
+      />
       {/* Card */}
-      <ProjectCard image={websiteImage} name="test" description="test" />
+      {filteredProject.map((project) => (
+        <ProjectCard
+          key={project.id}
+          image={project.image}
+          name={project.name}
+          description={project.description}
+          setCurrentIndex={setCurrentIndex}
+        />
+      ))}
     </div>
   );
 }
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
 
-export default function ProjectCard({ image, name, description }: any) {
+export default function ProjectCard({
+  image,
+  name,
+  description,
+  setCurrentIndex,
+}: any) {
   return (
-    <div className="flex flex-col bg-bgCard rounded-2xl border-2 border-borderCard shadow-xl overflow-hidden">
-      <Image src={image} />
+    <div className="flex flex-col bg-bgCard rounded-2xl border-2 border-borderCard shadow-2xl overflow-clip">
+      <Image src={image} alt={name} />
       <div className="px-4 pb-12 pt-4 space-y-1">
         <h3 className="text-2xl font-semibold text-darkText">name</h3>
         <figcaption className="text-justify line-clamp-2 text-textLightCard font-light">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. At neque
-          cupiditate tenetur eum, quia mollitia eveniet, doloremque distinctio
-          quasi nobis assumenda iste. Optio odit, nam pariatur repellat officiis
-          quam dolorem.
+          {description}
         </figcaption>
       </div>
     </div>
