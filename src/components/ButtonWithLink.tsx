ad960fc61b69a6be56878e753220bdd9b65e7bdb
Antoine Savreux <antoinesavreux06@gmail.com>
2024-02-16 14:45:39 +0100

    fix/homepage type of buttonIcons

--- src/components/ButtonWithLink.tsx
+++ src/components/ButtonWithLink.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
-import { Send, Download } from "lucide-react";
+import { Send, Download, Folder } from "lucide-react";
 
-const buttonIcons = {
+const buttonIcons: Record<string, JSX.Element> = {
   contact: <Send />,
   download: <Download />,
+  projects: <Folder />,
 };
 
 export default function ButtonWithLink({
@@ -13,12 +14,12 @@ export default function ButtonWithLink({
 }: {
   title: string;
   href: string;
-  type: string;
+  type: keyof typeof buttonIcons;
 }) {
   const icon = buttonIcons[type];
   return (
     <Link href={href}>
-      <button className="bg-primary px-6 py-3 text-xl font-semibold text-lightText rounded-full flex gap-2">
+      <button className="bg-primary px-6 py-3 text-xl font-semibold text-lightText rounded-full flex gap-2 items-center">
         {title}
         {icon}
       </button>
--- src/components/IntroTextButton.tsx
+++ src/components/IntroTextButton.tsx
@@ -4,7 +4,11 @@ export default function IntroTextButton({ text }: { text: string }) {
   return (
     <div className="flex flex-col items-center space-y-4">
       <p className="text-secondary">{text}</p>
-      <ButtonWithLink title="Voir tous mes projets" href="/projects" />
+      <ButtonWithLink
+        title="Voir tous mes projets"
+        href="/projects"
+        type="projects"
+      />
     </div>
   );
 }
