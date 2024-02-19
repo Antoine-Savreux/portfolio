b7bcd1ef699be11ed9fd1f9f45c02443eaa05972
Antoine Savreux <antoinesavreux06@gmail.com>
2024-02-19 09:42:32 +0100

    feat/homepage finished home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import Title from "@/components/Title";
 import About from "@/components/About";
 import IntroTextButton from "@/components/IntroTextButton";
 import Carousel from "@/components/Carousel";
-import { Send } from "lucide-react";
 
 export default function HomePage() {
   return (
