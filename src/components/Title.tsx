b588563924bc498a60890d0362ef05cdd021e947
Antoine Savreux <antoinesavreux06@gmail.com>
2024-02-19 11:03:45 +0100

    feat/aboutpage created about page

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,7 +2,7 @@ import { ArrowRightCircle } from "lucide-react";
 
 export default function Title({ title }: { title: string }) {
   return (
-    <div className="flex items-center space-x-2 justify-start w-full">
+    <div className="flex items-center space-x-2 justify-start w-full text-nowrap">
       <ArrowRightCircle size={34} className="text-secondary" />
       <h1 className="text-5xl font-bold text-secondary">{title}</h1>
     </div>
