01ac01ad7487e2d31bc9463cb7824523910391b8
Antoine Savreux <antoinesavreux06@gmail.com>
2024-02-19 12:08:05 +0100

    added target to link in projectCard

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function ProjectCard({ image, name, description, url }: any) {
   return (
-    <Link href={process.env.NEXT_PUBLIC_GITHUB_PERSONAL + url}>
+    <Link href={process.env.NEXT_PUBLIC_GITHUB_PERSONAL + url} target="blank">
       <div className="flex flex-col bg-bgCard rounded-2xl border-2 border-borderCard shadow-2xl overflow-clip h-80">
         <Image src={image} alt={name} className="h-3/5 object-cover" />
         <div className="px-4 pt-4 space-y-1 h-2/5">
