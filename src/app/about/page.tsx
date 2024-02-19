d49ec858e96829d01e14e065e74eacd108421a22
Antoine Savreux <antoinesavreux06@gmail.com>
2024-02-19 10:27:32 +0100

    feat/about created aout page structure

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import TitleTextSection from "@/components/TitleTextSection";
+
+export default function AboutPage() {
+  return (
+    <main className="min-h-full flex flex-col items-center p-8 space-y-8">
+      <TitleTextSection
+        title="Mon parcours"
+        text="J’ai grandi en Normandie, plus particulièrement près de la ville de Rouen. J’y ai fait mes études en commençant par un baccalauréat technique (sciences et technologies de l’industrie et du développement durable). Je me suis ensuite orienté vers le commerce avec un BTS management des unités commerciales. J’ai poursuivi dans le secteur de l’automobile par passion, mais je n’y ai pas trouvé mon bonheur. En parallèle, je me suis intéressé au web marketing et j’ai pu entamer une activité d’indépendant dans ce secteur. Cette porte m’a ouvert tout un univers sur le développement, et le web tout particulièrement. J’ai commencé en autodidacte par construire des sites no-code grâce à Shopify ou Wordpress, puis j’ai voulu rentrer dans la technique, j’ai suivi la formation de l’école O’clock en 2023. Je continu d’approfondir les technologies qui me tiennent à coeur en cherchant une opportunité pour mettre en pratique mes compétences pour cette année 2024."
+      />
+    </main>
+  );
+}
