f1f66c187892b00f121489f7f4f9b3bf024cfcf5
Antoine Savreux <antoinesavreux06@gmail.com>
2024-02-20 16:54:11 +0100

    feat/contactpage working sending email with nodemailer

---
+++ src/app/api/sendMessage/route.ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const buffers = [];
+    for await (const chunk of req.body) {
+      buffers.push(chunk);
+    }
+    const data = JSON.parse(Buffer.concat(buffers).toString());
+    const { name, email, message } = data;
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      secure: false,
+      auth: {
+        user: process.env.GMAIL_EMAIL,
+        pass: process.env.GOOGLE_APP_PASSWORD,
+      },
+    });
+
+    const mailOption = {
+      from: `portfolio: ${email}`,
+      to: "antoine.savreux06@gmail.com",
+      subject: `Portfolio - Message de contact de ${email}`,
+      html: `<h3>Nouveau message de portfolio</h3>
+      <p><strong>Message :</strong> ${message}</p>`,
+    };
+
+    await transporter.sendMail(mailOption);
+
+    return NextResponse.json(
+      { message: "Email envoyé avec succès" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Erreur lors de l'envoi du mail" },
+      { status: 500 }
+    );
+  }
+}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,29 @@
 "use client";
 
 import Title from "@/components/Title";
+import { useState } from "react";
 
 export default function ContactPage() {
-  const handleSubmit = (e: any) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
-    const name = e.target.name.value;
-    const email = e.target.email.value;
-    const message = e.target.message.value;
-    console.log(name, email, message);
+    try {
+      const response = await fetch("/api/sendMessage", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, email, message }),
+      });
+      if (!response.ok) {
+        throw new Error("Erreur lors de l'envoi de l'email");
+      }
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
   };
   return (
     <main className="min-h-full flex flex-col items-center p-8 space-y-8">
@@ -16,17 +31,31 @@ export default function ContactPage() {
       <form className="flex flex-col" onSubmit={handleSubmit}>
         <label htmlFor="name">
           Nom :
-          <input type="text" name="name" />
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </label>
         <label htmlFor="email">
           Email :
-          <input type="email" name="email" />
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <label htmlFor="message">
           Message :
-          <textarea name="message" />
+          <textarea
+            name="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
         </label>
-        <button type="submit">Envoyer</button>
+        <button>Envoyer</button>
       </form>
     </main>
   );
