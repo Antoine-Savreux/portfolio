0c0824028c90e99431fc7d9bf7a697349f24b054
Antoine Savreux <antoinesavreux06@gmail.com>
2024-02-22 10:40:53 +0100

    fix/logo replaced logopng by svg

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import logoImage from "@/../public/images/logo-light.png";
+import LogoSvg from "../../public/images/logo-light.svg";
 
 export default function Logo() {
-  return <Image src={logoImage} alt="logo" height={75} />;
+  return <Image src={LogoSvg} alt="logo" height={60} />;
 }
