72e505e986a635e875d27f5675d976820b09878a
Antoine Savreux <antoinesavreux06@gmail.com>
2024-02-22 15:12:19 +0100

    fix/title text changed to pretty to prevent shrinking for very tight screens

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,5 +1,5 @@
 export default function Title({ title }: { title: string }) {
   return (
-    <h1 className="text-5xl font-bold text-secondary text-nowrap">{title}</h1>
+    <h1 className="text-5xl font-bold text-secondary text-pretty">{title}</h1>
   );
 }
