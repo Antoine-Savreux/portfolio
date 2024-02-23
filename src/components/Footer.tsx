db7057d52e7aeeacd86d4b35eabcaf6ed1e5ff80
Antoine Savreux <antoinesavreux06@gmail.com>
2024-02-23 15:00:41 +0100

    feat/footer changed padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Github, Linkedin } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="bg-secondary flex flex-col items-center p-4 gap-2">
+    <footer className="bg-secondary flex flex-col items-center p-8 gap-2">
       <div className="flex space-x-2">
         <Github className=" text-primary" size={28} />
         <Linkedin className="text-primary" size={28} />
